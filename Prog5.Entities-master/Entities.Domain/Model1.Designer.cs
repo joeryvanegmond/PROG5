0e16f0b253937b5647747a14b9b8fa6577a55d06
joeryvanegmond <joeryvanegmond@hotmail.com>
2019-10-05 23:22:53 +0200

    test op macbook [werkt]
    Alleen de computernaam hoeft aangepast te worden in de app.config file in het mapje console en domain. Verder werkt het.

--- Prog5.Entities-master/Entities.Domain/Model1.Designer.cs
+++ Prog5.Entities-master/Entities.Domain/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\joery\Documents\Avans\PROG5\PROG5\Prog5.Entities-master\Entities.Domain\Model1.edmx'. 
+﻿// T4 code generation is enabled for model '\\Mac\Home\Documents\Avans\leerjaar 2\BLOK 5\PROG5\github\PROG5\Prog5.Entities-master\Entities.Domain\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
